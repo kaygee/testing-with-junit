596fe95c9fc7d289400d20866c94efdebb1a7af4
Kevin Gann <kevin.gann@rev.com>
2022-02-03 17:34:28 -0500

    Adding examples of exceptions.

--- src/main/java/annotations/BankAccount.java
+++ src/main/java/annotations/BankAccount.java
@@ -3,10 +3,20 @@ package annotations;
 public class BankAccount {
   private double balance;
   private double minimumBalance;
+  private boolean isActive = true;
+  private String holderName;
 
   public BankAccount(double balance, double minimumBalance) {
     this.balance = balance;
     this.minimumBalance = minimumBalance;
+  }
+
+  public String getHolderName() {
+    return holderName;
+  }
+
+  public void setHolderName(String holderName) {
+    this.holderName = holderName;
   }
 
   public double withdraw(double amount) {
@@ -18,12 +28,24 @@ public class BankAccount {
     }
   }
 
+  public boolean isActive() {
+    return isActive;
+  }
+
+  public void setActive(boolean active) {
+    isActive = active;
+  }
+
   public double deposit(double amount) {
     return balance += amount;
   }
 
   public double getBalance() {
     return balance;
+  }
+
+  public void setBalance(double balance) {
+    this.balance = balance;
   }
 
   public double getMinimumBalance() {
--- src/test/java/kg/BankAccountTest.java
+++ src/test/java/kg/BankAccountTest.java
@@ -4,8 +4,15 @@ import annotations.BankAccount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Test BankAccount Class")
 public class BankAccountTest {
@@ -19,7 +26,7 @@ public class BankAccountTest {
   }
 
   @Test
-  @DisplayName("Throw a RuntimeException if we try to withdraw too much")
+  @DisplayName("Throw a RuntimeException when withdrawing too much")
   public void testInvalidWithdrawThrowsRuntimeException() {
     BankAccount bankAccount = new BankAccount(500, 0);
     assertThrows(RuntimeException.class, () -> bankAccount.withdraw(1000));
@@ -31,5 +38,42 @@ public class BankAccountTest {
     BankAccount bankAccount = new BankAccount(400, 0);
     bankAccount.deposit(500);
     assertEquals(900, bankAccount.getBalance());
+  }
+
+  @Test
+  @DisplayName("Balance can become negative")
+  public void testWithdrawNotStuckAtZero() {
+    BankAccount bankAccount = new BankAccount(500, -1000);
+    bankAccount.withdraw(800);
+    assertNotEquals(0, bankAccount.getBalance());
+  }
+
+  @Test
+  @DisplayName("Test activation after creation")
+  public void testActive() {
+    BankAccount bankAccount = new BankAccount(500, -1000);
+    assertTrue(bankAccount.isActive());
+  }
+
+  @Test
+  @DisplayName("Test set account holder name")
+  public void testHolderNameSet() {
+    BankAccount bankAccount = new BankAccount(500, -1000);
+    bankAccount.setHolderName("Kevin");
+    assertNotNull(bankAccount.getHolderName());
+  }
+
+  @Test
+  @DisplayName("Test set account holder name")
+  public void tesDepositAndWithdrawWithoutExceptions() {
+    BankAccount bankAccount = new BankAccount(500, -1000);
+    assertAll(() -> bankAccount.deposit(200), () -> bankAccount.withdraw(200));
+  }
+
+  @Test
+  @DisplayName("Test speed of deposit")
+  public void testDepositTimeout() {
+    BankAccount bankAccount = new BankAccount(500, -1000);
+    assertTimeout(Duration.ofNanos(1), () -> bankAccount.deposit(200));
   }
 }
